61f585a92f23723bcb4cc47199c00cdb1552890f
PC <PC@Saad>
2024-10-28 21:36:32 +0300

    changed the implementation of the method countLeaves(BT<T> bt)

--- src/main/java/com/example/project/TreeUser.java
+++ src/main/java/com/example/project/TreeUser.java
@@ -1,23 +1,56 @@
 package main.java.com.example.project;
 
 public class TreeUser {
-    public static <T> int countLeaves(BT<T> bt) {
-    	if(bt.empty())
-        	return 0; 
-    	int count = 0;
-		LinkedStack<BTNode<T>> temp = new LinkedStack<>();
-		temp.push(bt.root);
-		while(!temp.empty()) {
-			BTNode<T> tempNode = temp.pop();
-			if(tempNode.right == null && tempNode.left == null)
-				count++;
-			else {
-				if(tempNode.left != null)
-					temp.push(tempNode.left);
-				if(tempNode.right != null)
-					temp.push(tempNode.right);
-				}
-		}
-		return count;
-    }
+	public static <T> int countLeaves(BT<T> bt) {
+		if (bt.empty())
+            return 0;
+
+        bt.find(Relative.Root);
+
+        if (bt.isLeaf())
+            return 1;
+
+        int count = 0;
+        LinkedStack<Character> directions = new LinkedStack<>();
+
+        while (true) {
+
+            if (bt.find(Relative.LeftChild)) {
+
+                directions.push('l');
+
+            } else {
+
+                if (bt.find(Relative.RightChild))
+                    directions.push('r');
+            }
+
+            if (bt.isLeaf()) {
+
+                count++;
+
+                while (true) {
+
+                    bt.find(Relative.Parent);
+
+                    char dir = directions.pop();
+
+                    if (dir == 'l') {
+
+                        if (bt.find(Relative.RightChild)) {
+
+                            directions.push('r');
+                            break;
+
+                        }
+
+                    }
+                        if (directions.empty())
+                            return count;
+                    }
+                }
+
+            }	
+	}
+
 }
